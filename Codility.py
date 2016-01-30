7de9172150d1c173c7a170b1abb7f9fd58982c7d
Rondou Chen <40and44sis@gmail.com>
2016-01-30 18:24:38 +0800

    add two test function

--- Codility.py
+++ Codility.py
@@ -46,9 +46,57 @@ def calculator (a = 0, b = 9):
     return a.count('0')*6+a.count('1')*2+a.count('2')*5+a.count('3')*5+a.count('4')*4+ \
         a.count('5')*5+a.count('6')*6+a.count('7')*3+a.count('8')*7+a.count('9')*6
 
+def Squidward():
+    q = input()
+    a = list(map(int, raw_input().split()))
+    s = 0
+    w = 0
+    for i, j in zip(a, sorted(a)):
+        s += j - i
+        w += not s
+
+    print w
+
+def sorteddd():
+
+    n, p, q, = map(int, raw_input().split())
+    s = raw_input()
+    a = []
+    if (n == p+q):
+        a.append(s[:p])
+        a.append(s[p:])
+    elif (n % min(p,q) == 0):
+        m = min(p,q)
+        while(s):
+            a.append(s[:m])
+            s = s[m:]
+    else:
+        big = max(p, q)
+        small = min(p, q)
+        while(s):
+            if (len(s) < big and len(s) < small):
+                print "-1"
+                exit()
+            if (len(s) % small == 0):
+                a.append(s[:small])
+                s = s[small:]
+            else:
+                if (len(s) < big):
+                    print "-1"
+                    exit()
+                a.append(s[:big])
+                s = s[big:]
+
+    print len(a)
+    for i in a:
+        print i
+#while (s):
+#    print s
 
 
-
+#print len(a)
+#for i in a:
+#    print i
 
 
 
